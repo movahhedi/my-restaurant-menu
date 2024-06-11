551ce5997d4aacfd748bad157a52861c1965b84a
Shahab Movahhedi <test@shmovahhedi.com>
2024-06-11 11:09:30 +0330

    feat: add menu code

---
+++ src/assets/scripts/MenuData.ts
@@ -0,0 +1,41 @@
+export const menuItems = {
+	appetizers: [
+		{ name: "Chicken Wings", price: "5.99" },
+		{ name: "Mozzarella Sticks", price: "4.99" },
+		{ name: "Onion Rings", price: "3.99" },
+	],
+	mainCourse: [
+		{
+			name: "Chicken Alfredo",
+			price: "12.99",
+			memo: "Fettuccine noodles and grilled chicken in a creamy Alfredo sauce.",
+		},
+		{ name: "Spaghetti", price: "10.99", memo: "Spaghetti noodles with marinara sauce." },
+		{
+			name: "Chicken Parmesan",
+			price: "13.99",
+			memo: "Breaded chicken breast topped with marinara sauce and melted cheese.",
+		},
+		{
+			name: "Lasagna",
+			price: "11.99",
+			memo: "Layered pasta with ricotta cheese, ground beef, and marinara sauce.",
+		},
+	],
+	chefSpecials: [
+		{ name: "Special 1", price: "15.99", memo: "Special 1 is a special dish that is special." },
+		{ name: "Special 2", price: "16.99", memo: "Special 2 is a special dish that is special." },
+		{ name: "Special 3", price: "17.99", memo: "Special 3 is a special dish that is special." },
+		{ name: "Special 4", price: "18.99", memo: "Special 4 is a special dish that is special." },
+	],
+	desserts: [
+		{ name: "Cheesecake", price: "4.99" },
+		{ name: "Chocolate Cake", price: "4.99" },
+		{ name: "Apple Pie", price: "3.99" },
+	],
+	drinks: [
+		{ name: "Soda", price: "1.99" },
+		{ name: "Iced Tea", price: "1.99" },
+		{ name: "Coffee", price: "1.99" },
+	],
+};
---
+++ src/assets/scripts/index.tsx
@@ -0,0 +1,90 @@
+import { menuItems } from "./MenuData";
+
+// interface IMenuItem_Props {
+// 	name: string;
+// 	price: string;
+// 	memo?: string;
+// }
+
+// function MenuItem(props: IMenuItem_Props) {
+// 	return (
+// 		<div class="item">
+// 			<h3 class="item-name">{props.name}</h3>
+// 			<p class="item-price">${props.price}</p>
+// 			<p class="item-desc">{props.memo}</p>
+// 		</div>
+// 	);
+// }
+
+interface IMenuItem {
+	name: string;
+	price: string;
+	memo?: string;
+	// memo: string | undefined;
+}
+
+interface IMenuItem_Props {
+	item: IMenuItem;
+}
+
+function MenuItem(props: IMenuItem_Props) {
+	return (
+		<div class="item">
+			<h3 class="item-name">{props.item.name}</h3>
+			<p class="item-price">${props.item.price}</p>
+			{props.item.memo && <p class="item-desc">{props.item.memo}</p>}
+		</div>
+	);
+}
+
+function MenuItem3(item: IMenuItem) {
+	return (
+		<div class="item">
+			<h3 class="item-name">{item.name}</h3>
+			<p class="item-price">${item.price}</p>
+			{item.memo && <p class="item-desc">{item.memo}</p>}
+		</div>
+	);
+}
+
+interface IMenuPart_Props {
+	title: string;
+	items: IMenuItem[];
+	special?: boolean;
+}
+
+function MenuPart(props: IMenuPart_Props) {
+	return (
+		<section class="menu-part">
+			<div class={["part-title", props.special && "special"]}>{props.title}.</div>
+			<div class="item-list">{props.items.map(MenuItem3)}</div>
+		</section>
+	);
+}
+
+const pageContent = (
+	<main id="main">
+		<section class="menu-part">
+			<h1 id="menu-title">
+				<span>ME</span>
+				<span>NU</span>
+			</h1>
+		</section>
+
+		<MenuPart title="Appetizers" items={menuItems.appetizers} />
+		<MenuPart title="Main Course" items={menuItems.mainCourse} />
+		<MenuPart title="Chef Specials" items={menuItems.chefSpecials} special />
+		<MenuPart title="Desserts" items={menuItems.desserts} />
+		<MenuPart title="Drinks" items={menuItems.drinks} />
+
+		<section id="footer" class="menu-part">
+			<p>YOUR LOGO</p>
+
+			<p>1234 Main St. | City, State 12345</p>
+
+			<p>myfancyrestaurant.com</p>
+		</section>
+	</main>
+);
+
+document.body.appendChild(pageContent);
